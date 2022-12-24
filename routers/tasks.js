601b32b34bb73f83c7d7db65777d26220bfcf9d3
Emmanuel Omoogun <abiodunebun12@gmail.com>
2022-12-24 12:20:32 +0100

    CRUD tasks-via-task-id and Update routers-task

--- routers/tasks.js
+++ routers/tasks.js
@@ -17,3 +17,90 @@ router.post('/tasks', auth, async (req, res) => {
         res.status(400).send(e);
     }
 });
+
+// Read a task
+router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+    try {
+        await req.user
+            .populate({
+                path: 'tasks',
+                match,
+                options: {
+                    limit: parseInt(req.query.limit),
+                    skip: parseInt(req.query.skip),
+                    sort,
+                },
+            })
+            .execPopulate();
+        res.send(req.user.tasks);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+// Read tasks using task id
+router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const task = await Task.findOne({ _id, owner: req.user._id });
+        if (!task) {
+            return res.status(401).send({ error: 'Task id not found' });
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+// Update tasks using task id
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['Task', 'completed'];
+    const isValidOperation = updates.every((update) =>
+        allowedUpdates.includes(update)
+    );
+    if (!isValidOperation) {
+        return res.status(401).send({ error: 'Invalid updates' });
+    }
+    try {
+        const task = await Task.findOne({
+            _id: req.params.id,
+            owner: req.user._id,
+        });
+        if (!task) {
+            res.send({ error: 'Task id not found to update' });
+        }
+        updates.forEach((update) => (task[update] = req.body[update]));
+        await task.save();
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+// Delete tasks using task id
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({
+            _id: req.params.id,
+            owner: req.user._id,
+        });
+        if (!task) {
+            res.status404.send({ error: 'Task id not found' });
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send({ e: 'Catch Error', e });
+    }
+});
+
+module.exports = router;
